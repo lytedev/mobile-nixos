b2c496bbcebc85a28d1d939b56bd331536bd1ac4
Daniel Flanagan <daniel@lyte.dev>
2024-10-08 16:32:37 -0500

    fix: config.sound was hard deprecated

--- devices/pine64-pinephone/sound.nix
+++ devices/pine64-pinephone/sound.nix
@@ -2,17 +2,18 @@
 #
 # A hack to avoid mass rebuilds of packages that depend on
 # alsaLib.
-
-{ config, lib, ... }:
-
-let
+{
+  config,
+  lib,
+  ...
+}: let
   # From https://gitlab.manjaro.org/manjaro-arm/packages/community/phosh/alsa-ucm-pinephone
   ucm2 = "${./ucm2}";
 in {
   config = lib.mkMerge [
-    (lib.mkIf config.sound.enable {
+    {
       environment.variables.ALSA_CONFIG_UCM2 = ucm2;
-    })
+    }
     (lib.mkIf (config.hardware.pulseaudio.enable && !config.hardware.pulseaudio.systemWide) {
       systemd.user.services.pulseaudio.environment.ALSA_CONFIG_UCM2 = ucm2;
     })
